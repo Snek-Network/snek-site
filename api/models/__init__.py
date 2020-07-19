8732bf284a5c84eccd4f33d71a5a997f9cc9f582
Den4200 <dennis@dennispham.me>
2020-07-19 16:07:24 -0400

    Create GuildConfig model

--- api/models/__init__.py
+++ api/models/__init__.py
@@ -1,5 +1,5 @@
-from api.models.guild import Guild
+from api.models.guild import Guild, GuildConfig
 from api.models.role import Role
 from api.models.user import User
 
-__all__ = ('Guild', 'Role', 'User')
+__all__ = ('Guild', 'GuildConfig', 'Role', 'User')
