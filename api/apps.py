720b0c66c60b24305713446afc05ac61a200c709
Dennis Pham <dennis@dennispham.me>
2020-07-20 20:31:33 +0000

    Import signals once API is loaded and ready

--- api/apps.py
+++ api/apps.py
@@ -3,3 +3,7 @@ from django.apps import AppConfig
 
 class ApiConfig(AppConfig):
     name = 'api'
+
+    def ready(self):
+        """Run when the app has been loaded and is ready to serve requests."""
+        import api.signals  # NOQA: F401
