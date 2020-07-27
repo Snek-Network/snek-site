7af9677f37d4dce80077aa4033165d6537e2373c
Dennis Pham <dennis@dennispham.me>
2020-07-27 04:22:59 +0000

    Set ALLOWED_HOSTS for debugging

--- snek_site/settings.py
+++ snek_site/settings.py
@@ -28,11 +28,16 @@ SECRET_KEY = env('SECRET_KEY')
 DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = [
-    '127.0.0.1',
-    'localhost',
     'sneknetwork.com',
     'www.sneknetwork.com'
 ]
+
+if DEBUG:
+    ALLOWED_HOSTS += [
+        '127.0.0.1',
+        'localhost',
+        'snek_site'
+    ]
 
 
 # Application definition
